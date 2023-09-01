073552f1951698826f9d34915c782c1fd0b0437a
rgw <rgw5267@gmail.com>
2023-09-01 11:19:14 +0800

    lpuart msp

--- stm32_drivers/stm32_lpuart_l0xx.c
+++ stm32_drivers/stm32_lpuart_l0xx.c
@@ -11,35 +11,37 @@
 #define DBG_LVL DBG_LOG
 #include "sdk_log.h"
 
+extern sdk_uart_t lpuart;
+
+__WEAK int stm32_lpuart_msp_init(sdk_uart_t *uart)
+{
+    return -SDK_ERROR;
+}
+
+__WEAK int stm32_lpuart_msp_deinit(sdk_uart_t *uart)
+{
+    return -SDK_ERROR;
+}
+
 static int32_t stm32_lpuart_open(sdk_uart_t *uart, int32_t baudrate, int32_t data_bit, char parity, int32_t stop_bit)
 {
     LL_LPUART_InitTypeDef LPUART_InitStruct = {0};
 
-    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+    // msp init
+    if (stm32_lpuart_msp_init(uart) != SDK_OK)
+    {
+        return -SDK_ERROR;
+    }
 
-    /* Peripheral clock enable */
-    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_LPUART1);
-
-    LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOB);
-    /**LPUART1 GPIO Configuration
-    PB10   ------> LPUART1_TX
-    PB11   ------> LPUART1_RX
-    */
-    GPIO_InitStruct.Pin = LL_GPIO_PIN_10;
-    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-    GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
-    LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = LL_GPIO_PIN_11;
-    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-    GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
-    LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    if(uart->instance == LPUART1)
+    {
+        /* Peripheral clock enable */
+        LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_LPUART1);
+    }
+    else
+    {
+        return -SDK_ERROR;
+    }
 
     LPUART_InitStruct.BaudRate = baudrate;
 
@@ -84,27 +86,31 @@ static int32_t stm32_lpuart_open(sdk_uart_t *uart, int32_t baudrate, int32_t dat
 
     LPUART_InitStruct.TransferDirection = LL_LPUART_DIRECTION_TX_RX;
     LPUART_InitStruct.HardwareFlowControl = LL_LPUART_HWCONTROL_NONE;
-    LL_LPUART_Init(LPUART1, &LPUART_InitStruct);
+    LL_LPUART_Init(uart->instance, &LPUART_InitStruct);
 
 
-    LL_LPUART_Enable(LPUART1);
+    LL_LPUART_Enable(uart->instance);
 
     return SDK_OK;
 }
 
 static int32_t stm32_lpuart_close(sdk_uart_t *uart)
 {
-    LL_LPUART_DeInit(LPUART1);
-    LL_LPUART_Disable(LPUART1);
-    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_10, LL_GPIO_MODE_ANALOG);
-    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_11, LL_GPIO_MODE_ANALOG);
+    LL_LPUART_DeInit(uart->instance);
+    LL_LPUART_Disable(uart->instance);
+
+    // msp deinit
+    if (stm32_lpuart_msp_deinit(uart) != SDK_OK)
+    {
+        return -SDK_ERROR;
+    }
     return SDK_OK;
 }
 
 static int32_t stm32_lpuart_putc(sdk_uart_t *uart, int32_t ch)
 {
-    while(0 == LL_LPUART_IsActiveFlag_TXE(LPUART1));
-    LL_LPUART_TransmitData8(LPUART1, (uint8_t) ch);
+    while(0 == LL_LPUART_IsActiveFlag_TXE(uart->instance));
+    LL_LPUART_TransmitData8(uart->instance, (uint8_t) ch);
     return ch;
 }
 
@@ -113,8 +119,8 @@ static int32_t stm32_lpuart_getc(sdk_uart_t *uart)
     int ch;
 
     ch = -1;
-    if (LL_LPUART_IsActiveFlag_RXNE(LPUART1) != 0)
-        ch = LL_LPUART_ReceiveData8(LPUART1);
+    if (LL_LPUART_IsActiveFlag_RXNE(uart->instance) != 0)
+        ch = LL_LPUART_ReceiveData8(uart->instance);
     return ch;
 }
 
@@ -123,39 +129,37 @@ static int32_t stm32_lpuart_control(sdk_uart_t *uart, int32_t cmd, void *args)
     switch (cmd)
     {
     case SDK_CONTROL_UART_DISABLE_INT:
-        LL_LPUART_DisableIT_RXNE(LPUART1);
-        LL_LPUART_DisableIT_ERROR(LPUART1);
-        NVIC_DisableIRQ(LPUART1_IRQn);
+        LL_LPUART_DisableIT_RXNE(uart->instance);
+        LL_LPUART_DisableIT_ERROR(uart->instance);
+        NVIC_DisableIRQ(uart->irq);
         break;
     case SDK_CONTROL_UART_ENABLE_INT:
-        NVIC_SetPriority(LPUART1_IRQn, 1);
-        NVIC_EnableIRQ(LPUART1_IRQn);
-        LL_LPUART_EnableIT_RXNE(LPUART1);
-        LL_LPUART_EnableIT_ERROR(LPUART1);
+        NVIC_SetPriority(uart->irq, uart->irq_prio);
+        NVIC_EnableIRQ(uart->irq);
+        LL_LPUART_EnableIT_RXNE(uart->instance);
+        LL_LPUART_EnableIT_ERROR(uart->instance);
         break;
     case SDK_CONTROL_UART_INT_IDLE_ENABLE:
         /*wait IDLEF set and clear it*/
-        while (0 == LL_LPUART_IsActiveFlag_IDLE(LPUART1))
+        while (0 == LL_LPUART_IsActiveFlag_IDLE(uart->instance))
         {
         }
-        LL_LPUART_ClearFlag_IDLE(LPUART1);
-        LL_LPUART_EnableIT_IDLE(LPUART1);
+        LL_LPUART_ClearFlag_IDLE(uart->instance);
+        LL_LPUART_EnableIT_IDLE(uart->instance);
         break;
     case SDK_CONTROL_UART_INT_IDLE_DISABLE:
-        LL_LPUART_DisableIT_IDLE(LPUART1);
+        LL_LPUART_DisableIT_IDLE(uart->instance);
         break;
     case SDK_CONTROL_UART_ENABLE_RX:
-        LL_LPUART_EnableDirectionRx(LPUART1);
+        LL_LPUART_EnableDirectionRx(uart->instance);
         break;
     case SDK_CONTROL_UART_DISABLE_RX:
-        LL_LPUART_DisableDirectionRx(LPUART1);
+        LL_LPUART_DisableDirectionRx(uart->instance);
         break;
     }
 
     return SDK_OK;
 }
-
-extern sdk_uart_t lpuart;
 
 void LPUART1_IRQHandler(void)
 {
@@ -193,6 +197,9 @@ __WEAK void lpuart_wakeup_callback(void)
 
 sdk_uart_t lpuart = 
 {
+    .instance = LPUART1,
+    .irq = LPUART1_IRQn,
+    .irq_prio = 1,
     .ops.open = stm32_lpuart_open,
     .ops.close = stm32_lpuart_close,
     .ops.putc = stm32_lpuart_putc,
